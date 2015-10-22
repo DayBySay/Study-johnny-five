1a1fc2e136aaa16775b9fd3744c670de9eba37f7
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-10-22 21:57:13 +0900

    関数名ミスってたので修正

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-    var led = new five.led(13);
-    led.blink(500)
+    var led = new five.Led(13);
+    led.blink(1000);
 });
